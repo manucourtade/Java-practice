1b329d1a501721c1b4f63e4010291ba4c74ccd77
manucourtade <courtademanuel@outlook.es>
2026-03-18 21:47:17 -0300

    I agree more methods and bubble sort

--- Alumno/src/Alumno.java
+++ Alumno/src/Alumno.java
@@ -61,6 +61,17 @@ class Curso {
         }
     }
 
+    int cantidadAprobado () {
+        int suma = 0;
+
+        for (int i = 0; i < alumnos.size(); i++) {
+            if (alumnos.get(i).aprobado()) {
+                suma ++;
+            }
+        }
+        return suma;
+    }
+
     void mostrarDesaprobados () {
         for (int i = 0; i < alumnos.size(); i++) {
             if (!alumnos.get(i).aprobado()) {
@@ -69,17 +80,58 @@ class Curso {
         }
     }
 
-    void mejorAlumno() {
+    void mejorpeorAlumno() {
         Alumno mejor = alumnos.get(0);
+        Alumno peor = alumnos.get(0);
 
         for (int i = 1; i < alumnos.size(); i++) {
-            if (alumnos.get(i).promedio() > mejor.promedio() ) {
+            if (alumnos.get(i).promedio() > mejor.promedio()) {
                 mejor = alumnos.get(i);
-
             }
+            if (alumnos.get(i).promedio() < peor.promedio()) {
+                peor = alumnos.get(i);
+            }
+
         }
         System.out.println("El alumno " + mejor.getNombre() + " Tiene el mejor promedio con " + mejor.promedio());
+        System.out.println("El alumno " + peor.getNombre() + " Tiene el peor promedio con " + peor.promedio());
     }
+
+    Alumno buscarAlumno(String nombre) {
+        for (int i = 0; i < alumnos.size(); i++) {
+            if (alumnos.get(i).getNombre().equals(nombre)) {
+                return alumnos.get(i);
+            }
+        }
+        return null;
+    }
+
+    void promedioGeneral () {
+        double suma = 0;
+        for (int i = 0; i < alumnos.size(); i++) {
+            suma+= alumnos.get(i).promedio();
+        }
+        double promedioGen = suma / alumnos.size();
+        if (alumnos.size() == 0) {
+            System.out.println("No hay alumnos");
+            return;
+        }
+        System.out.println("El promedio general del curso es de: " + promedioGen);
+    }
+
+    void ordenarCurso () {
+        int cant = alumnos.size();
+        for (int i = 0; i < cant; i++) {
+            for (int j = 0; j < cant - 1; j++) {
+                if (alumnos.get(j).promedio() < alumnos.get(j + 1).promedio()) {
+                    Alumno aux = alumnos.get(j);
+                    alumnos.set(j, alumnos.get(j + 1));
+                    alumnos.set(j + 1,aux);
+                }
+            }
+        }
+    }
+
 }
 
 class Main {
@@ -95,9 +147,21 @@ class Main {
         curso.agregarAlumno(a3);
 
         //curso.mostrarAlumnos();
-        curso.mostrarAprobados();
-        curso.mostrarDesaprobados();
-        curso.mejorAlumno();
+        //curso.mostrarAprobados();
+        //curso.mostrarDesaprobados();
+        //curso.mejorpeorAlumno();
+        curso.promedioGeneral();
+        //int cant = curso.cantidadAprobado();
+        //System.out.println("Hay " + cant + " alumnos aprobados!");
+        curso.ordenarCurso();
+        curso.mostrarAlumnos();
+
+        /*Alumno encontrado = curso.buscarAlumno("Manuel");
+        if (encontrado != null) {
+            System.out.println("Alumno encontrado: " + encontrado);
+        } else  {
+            System.out.println("No se encontro");
+        }*/
 
     }
 }
